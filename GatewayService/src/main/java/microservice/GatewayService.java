3101ab4aaee454db95920078a9a4e90ffab1ffab
Aluis Marte <aluismarte@gmail.com>
2024-07-16 00:31:30 -0400

    Adding main class to gateway service

---
+++ GatewayService/src/main/java/microservice/GatewayService.java
@@ -0,0 +1,12 @@
+package microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayService.class, args);
+    }
+}
