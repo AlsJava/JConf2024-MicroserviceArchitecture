a4172cb8e45a0f110699139648025e175e5e6bfb
Aluis Marte <aluismarte@gmail.com>
2024-07-19 15:18:16 -0400

    Fixing script

--- Others/initKafka.sh
+++ Others/initKafka.sh
@@ -1,5 +1,6 @@
 wget https://downloads.apache.org/kafka/3.7.1/kafka_2.13-3.7.1.tgz
 tar -xzf kafka_2.13-3.7.1.tgz
+mv kafka_2.13-3.7.1/ kafka/
 cd kafka/
 KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
 bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
